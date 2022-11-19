9edaed9c9d48394ae57e672a8ddcd33c50197ce6
CristiPV <cristi.valentin29@gmail.com>
2022-11-19 20:51:12 +0100

    refactor code

--- libs/interfaces/src/lib/luggage.interface.ts
+++ libs/interfaces/src/lib/luggage.interface.ts
@@ -16,6 +16,20 @@ export interface ILuggage extends ICompletedAt {
   bbOut?: string; // Who gave the luggage to the guest.
 }
 
+export interface ICreateRequest {
+  luggageType: LuggageType;
+  roomReady?: boolean;
+  room: string;
+  name: string;
+  arrivalTime?: Date;
+  bags: string;
+  description: string;
+  tagNr: string;
+  bbDown: string; // Who took the luggage from the guest.
+  bbLr: string; // Who put the luggage in the storage room.
+  bbOut?: string; // Who gave the luggage to the guest.
+}
+
 export interface IUpdateRequest {
   luggageId?: string;
   luggageType?: LuggageType;
@@ -30,18 +44,4 @@ export interface IUpdateRequest {
   bbLr?: string;
   bbOut?: string;
   completedAt?: string;
-}
-
-export interface ICreateRequest {
-  luggageType: LuggageType;
-  roomReady?: boolean;
-  room: string;
-  name: string;
-  arrivalTime?: Date;
-  bags: string;
-  description: string;
-  tagNr: string;
-  bbDown: string; // Who took the luggage from the guest.
-  bbLr: string; // Who put the luggage in the storage room.
-  bbOut?: string; // Who gave the luggage to the guest.
 }
