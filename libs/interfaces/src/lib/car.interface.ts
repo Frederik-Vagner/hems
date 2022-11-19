68895ccba6e1734af254c03bc6e515050fa95755
Frederik-Vagner <70664117+Frederik-Vagner@users.noreply.github.com>
2022-11-19 13:28:54 +0100

    add car interface and location enum

---
+++ libs/interfaces/src/lib/car.interface.ts
@@ -0,0 +1,21 @@
+import { ICompletedAt } from './base.interface';
+import { Location } from './location.enum';
+
+export interface ICar extends ICompletedAt {
+    carId: string;
+    tagNr: string;
+    room: string;
+    arrivialDate: Date;
+    departureDate: Date;
+    name: string;
+    licensePlate: string;
+    expirationDate?: Date;
+    pickupTime?: Date;
+    deliveryTime?: Date;
+    bbDown?: string;
+    bbUp?: string;
+    location: Location;
+    parkingLot?: string;
+    comments: string;
+    charged: boolean;
+}
