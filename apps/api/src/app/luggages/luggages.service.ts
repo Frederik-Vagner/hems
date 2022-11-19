2992424512ce1a656dfb4c9ebc1946f41a4eefb9
CristiPV <cristi.valentin29@gmail.com>
2022-11-19 18:25:14 +0100

    add luggages service

---
+++ apps/api/src/app/luggages/luggages.service.ts
@@ -0,0 +1,31 @@
+import { LuggageType } from '@hems/interfaces';
+import { Luggage } from '@hems/models';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, Between } from 'typeorm';
+
+@Injectable()
+export class LuggagesService {
+  constructor(
+    @InjectRepository(Luggage)
+    private readonly luggageRepo: Repository<Luggage>
+  ) {}
+
+  async findAllByLuggageTypeAndCreatedAt(
+    luggageType: LuggageType,
+    createdAt: Date
+  ) {
+    return this.luggageRepo.find({
+      where:
+        luggageType === LuggageType.LONG_TERM
+          ? null
+          : {
+              luggageType,
+              createdAt: Between<Date>(
+                new Date(createdAt.setUTCHours(0, 0, 0, 0)),
+                new Date(createdAt.setUTCHours(23, 59, 59, 999))
+              ),
+            },
+    });
+  }
+}
