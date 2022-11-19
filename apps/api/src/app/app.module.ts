ad518e21de37e5d91da994f1e3f014b1287537a9
Kwandes <bz.jasiek@gmail.com>
2022-11-19 16:09:10 +0100

    Disable auth

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
-import { AuthModule } from './auth/auth.module';
 import { configService } from './config/config.service';
 import { LoggerMiddleware } from './middleware/logging.middleware';
 import { UsersModule } from './users/users.module';
@@ -10,7 +9,6 @@ import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
-    AuthModule,
     UsersModule,
   ],
   controllers: [AppController],
