e123eabad81a7a995c20b9709a50ce479c217467
Teodor25 <teodor_jonasson@hotmail.com>
2022-11-20 18:41:39 +0100

    feat: 🎸 assignment page mock data

--- apps/hems/src/app/pages/assignments/assignments-page.component.ts
+++ apps/hems/src/app/pages/assignments/assignments-page.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { IAssignment } from '@hems/interfaces';
+import { assignments } from './mockAssignment';
 
 @Component({
   selector: 'hems-assignments-page',
   templateUrl: './assignments-page.component.html',
   styleUrls: ['./assignments-page.component.scss'],
 })
-export class AssignmentsPageComponent {}
+export class AssignmentsPageComponent {
+  assignmentList: IAssignment[] = assignments;
+
+  assignmentColumns = [
+    'room',
+    'task',
+    'comments',
+    'receivedBy',
+    'receivedAt',
+    'performedBy',
+    'completedAt'
+  ]
+  
+  dateDisplay(date: Date) {
+    return date.toLocaleString(undefined, {hour: '2-digit', hour12: false, minute:'2-digit'})
+  }
+}
---
+++ apps/hems/src/app/pages/assignments/mockAssignment.ts
@@ -0,0 +1,34 @@
+import { IAssignment } from "@hems/interfaces";
+
+export const assignments: IAssignment[] = [
+    {
+      assignmentId: "string",
+      room: "123",
+      task: "RA",
+      comments: "I DID STUF IM PRUD OF MYSELF YAY WOHOOO",
+      receivedBy: "Reception",
+      performedBy: "FV",
+      receivedAt: new Date(),
+      completedAt: new Date(),
+    },
+    {
+      assignmentId: "string",
+      room: "120",
+      task: "RA",
+      comments: "I DID STUF IM PRUD OF MYSELF YAY WOHOOO",
+      receivedBy: "Reception",
+      performedBy: "AM",
+      receivedAt: new Date(),
+      completedAt: new Date(),
+    },
+    {
+      assignmentId: "string",
+      room: "121",
+      task: "RA",
+      comments: "I DID STUF IM PRUD OF MYSELF YAY WOHOOO",
+      receivedBy: "Guest",
+      performedBy: "FV",
+      receivedAt: new Date(),
+      completedAt: new Date(),
+    },
+  ]
