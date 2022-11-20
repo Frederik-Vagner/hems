b0386e89344cfafc7e09c37d5e6c14b36368ba20
CristiPV <cristi.valentin29@gmail.com>
2022-11-20 12:17:40 +0100

    add create and update endpoints

--- apps/api/src/app/cars/cars.service.ts
+++ apps/api/src/app/cars/cars.service.ts
@@ -1,3 +1,4 @@
+import { CreateCarRequest, UpdateCarRequest } from '@hems/interfaces';
 import { Car } from '@hems/models';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -21,5 +22,19 @@ export class CarsService {
     });
   }
 
+  async createCar(carData: CreateCarRequest) {
+    return await this.carRepo.save(carData);
+  }
 
+  async updateCar(carId: string, carData: UpdateCarRequest) {
+    const car = await this.carRepo.findOneByOrFail({ carId });
+
+    for (const key in carData) {
+      if (Object.prototype.hasOwnProperty.call(carData, key)) {
+        car[key] = carData[key];
+      }
+    }
+
+    return await this.carRepo.save(car);
+  }
 }
