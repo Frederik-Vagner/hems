7da3a489c25af9e53f9ddde73a83ad9d8dac235a
Kwandes <bz.jasiek@gmail.com>
2022-11-26 22:31:09 +0100

    Update checkin time of arrival formatting

--- apps/hems/src/app/pages/checkin/checkin.component.ts
+++ apps/hems/src/app/pages/checkin/checkin.component.ts
@@ -152,14 +152,14 @@ export class CheckinComponent implements OnInit {
     });
   }
 
-  formatDate(date: string): string {
+  formatDate(date: string, timeOnly: boolean = false): string {
     if (!date) {
       return '';
     }
     const parsedDate = new Date(date);
     return parsedDate.toLocaleString(undefined, {
-      month: '2-digit',
-      day: '2-digit',
+      month: timeOnly ? undefined : '2-digit',
+      day: timeOnly ? undefined : '2-digit',
       hour: '2-digit',
       hour12: false,
       minute: '2-digit',
