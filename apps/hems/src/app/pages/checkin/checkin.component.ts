33d76d374d91f641a0370a6e64bb167c40e0e2c4
Kwandes <bz.jasiek@gmail.com>
2022-11-26 19:51:21 +0100

    Add date formatting

--- apps/hems/src/app/pages/checkin/checkin.component.ts
+++ apps/hems/src/app/pages/checkin/checkin.component.ts
@@ -152,4 +152,18 @@ export class CheckinComponent implements OnInit {
       width: '500px',
     });
   }
+
+  formatDate(date: string): string {
+    if (!date) {
+      return '';
+    }
+    const parsedDate = new Date(date);
+    return parsedDate.toLocaleString(undefined, {
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      hour12: false,
+      minute: '2-digit',
+    });
+  }
 }
