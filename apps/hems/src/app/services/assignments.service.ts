dda04ee14b75cf02cb9c816d20e67a94a0489290
Teodor25 <teodor_jonasson@hotmail.com>
2022-11-26 15:07:49 +0100

    feat: 🎸 assignments services

--- apps/hems/src/app/services/assignments.service.ts
+++ apps/hems/src/app/services/assignments.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { IAssignment, ILuggage } from '@hems/interfaces';
+import { CreateAssignmentRequest, IAssignment, ILuggage, UpdateAssignmentRequest } from '@hems/interfaces';
 import { Observable } from 'rxjs';
 import { environment as env } from '../../environments/environment';
 
@@ -10,21 +10,45 @@ import { environment as env } from '../../environments/environment';
 export class AssignmentsService {
   constructor(private http: HttpClient) {}
 
+  /**
+   * Calls the API to get a list of assignments.
+   *
+   * @returns an observable with a list of IAssignments.
+   */
   public getAssignments(createdAt: Date): Observable<IAssignment[]> {
     return this.http.get<IAssignment[]>(
       `${env.apiUrl}/assignments?createdAt=${createdAt.toISOString()}`
     );
   }
 
-  public getCheckout(createdAt: Date): Observable<ILuggage[]> {
-    return this.http.get<ILuggage[]>(
-      `${env.apiUrl}/luggages/checkout?createdAt=${createdAt.toISOString()}`
-    );
+  /**
+   * Calls the API to create a assignment.
+   *
+   * @param params - a create assignment request with all the needed info.
+   * @returns an observable with created IAssignment.
+   */
+  public createAssignment(params: CreateAssignmentRequest): Observable<IAssignment> {
+    return this.http.post<IAssignment>(`${env.apiUrl}/assignments`, params);
   }
 
-  public getLongTerm(createdAt: Date): Observable<ILuggage[]> {
-    return this.http.get<ILuggage[]>(
-      `${env.apiUrl}/luggages/longTerm?createdAt=${createdAt.toISOString()}`
-    );
+  /**
+   * Calls the API to create a assignment.
+   *
+   * @param id ID fo the assignment to update.
+   * @param params - an UpdateAssignmentRequest object to update the assignment.
+   * @returns an observable with updated assignment.
+   */
+  public updateAssignment(id: string, params: UpdateAssignmentRequest): Observable<IAssignment> {
+    return this.http.post<IAssignment>(`${env.apiUrl}/assignments/${id}`, params);
+  }
+
+  /**
+   * Calls the API to fetch an assignment by ID.
+   *
+   * @param id - the ID of the assignment.
+   * @returns an observable with the requested assignment.
+   */
+  public getAssignmentById(id: string): Observable<IAssignment> {
+    return this.http.get<IAssignment>(`${env.apiUrl}/assignments/${id}`);
   }
 }
