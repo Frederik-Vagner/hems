c27ab1a731a1d037d969621f9d11f43d8106205d
CristiPV <cristi.valentin29@gmail.com>
2022-11-26 14:31:27 +0100

    group task files in folder

--- libs/interfaces/src/index.ts
+++ libs/interfaces/src/index.ts
@@ -3,7 +3,7 @@ export * from './lib/auth.dto';
 export * from './lib/luggage/luggage.dto';
 export * from './lib/car/car.dto';
 export * from './lib/assignment/assignment.dto';
-export * from './lib/task.dto';
+export * from './lib/task/task.dto';
 
 // Interfaces
 export * from './lib/auth.interface';
@@ -11,7 +11,7 @@ export * from './lib/base.interface';
 export * from './lib/local-storage';
 export * from './lib/user.interface';
 export * from './lib/luggage/luggage.interface';
-export * from './lib/task.interface';
+export * from './lib/task/task.interface';
 export * from './lib/car/car.interface';
 export * from './lib/assignment/assignment.interface';
 
