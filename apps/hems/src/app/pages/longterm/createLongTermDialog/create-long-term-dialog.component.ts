1f6be1d455fd76327f14f9fa6d8018c23a83fc05
Teodor25 <teodor_jonasson@hotmail.com>
2022-11-29 05:50:54 +0100

    feat: 🎸 Initialize dialogs

---
+++ apps/hems/src/app/pages/longterm/createLongTermDialog/create-long-term-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hems-create-long-term-dialog',
+  templateUrl: './create-long-term-dialog.component.html',
+  styleUrls: [
+    '../../../../assets/checkbox.scss',
+    '../../../../assets/dialog.scss',
+  ],
+})
+export class CreateLongTermDialogComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
---
+++ apps/hems/src/app/pages/longterm/updateLongTermDialog/update-long-term-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hems-update-long-term-dialog',
+  templateUrl: './update-long-term-dialog.component.html',
+  styleUrls: [
+    '../../../../assets/checkbox.scss',
+    '../../../../assets/dialog.scss',
+  ],
+})
+export class UpdateLongTermDialogComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
