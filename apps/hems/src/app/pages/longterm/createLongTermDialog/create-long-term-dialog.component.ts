4e3c9c5c04d86bb8e27607069d46bd368d6874d9
Teodor25 <teodor_jonasson@hotmail.com>
2022-11-29 09:14:02 +0100

    feat: 🎸 Create long term function

--- apps/hems/src/app/pages/longterm/createLongTermDialog/create-long-term-dialog.component.ts
+++ apps/hems/src/app/pages/longterm/createLongTermDialog/create-long-term-dialog.component.ts
@@ -1,9 +1,13 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import {
   UntypedFormControl,
   UntypedFormGroup,
   Validators,
 } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LuggageType } from '@hems/interfaces';
 import { LuggageService } from '../../../services/luggage.service';
 
 @Component({
@@ -19,11 +23,14 @@ export class CreateLongTermDialogComponent implements OnInit {
   guestHasApproved = false;
   maxDatetime = new Date();
 
-  constructor(private luggageService: LuggageService) {}
+  constructor(
+    private luggageService: LuggageService,
+    private snackbar: MatSnackBar,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.createLongTermForm = new UntypedFormGroup({
-      dateIn: new UntypedFormControl('', [Validators.required]),
       room: new UntypedFormControl('', [
         Validators.required,
         Validators.maxLength(10),
@@ -36,17 +43,52 @@ export class CreateLongTermDialogComponent implements OnInit {
       dateNeeded: new UntypedFormControl('', []),
       bbLr: new UntypedFormControl('', [Validators.required]),
       location: new UntypedFormControl('', [Validators.required]),
-      bbOut: new UntypedFormControl('', [Validators.required]),
-      dateOut: new UntypedFormControl('', []),
+      // bbOut: new UntypedFormControl('', [Validators.required]),
+      // dateOut: new UntypedFormControl('', []),
     });
   }
 
   onSubmit(): void {
-    console.log('skrt');
+    if (!this.guestHasApproved) {
+      this.snackbar.open('Guest needs to approve storing their data.', 'Okay', {
+        duration: 10000,
+      });
+      return;
+    } else {
+      this.createLongTermEntry();
+    }
   }
 
   createLongTermEntry(): void {
-    // this.luggageService.create({
-    // }).subscribe()
+    this.luggageService
+      .create({
+        room: this.createLongTermForm.get('room')?.value,
+        roomReady: false,
+        name: this.createLongTermForm.get('name')?.value,
+        bags: this.createLongTermForm.get('bags')?.value,
+        description: this.createLongTermForm.get('comments')?.value,
+        tagNr: this.createLongTermForm.get('tagNr')?.value,
+        arrivalTime: this.createLongTermForm.get('dateNeeded')?.value,
+        bbLr: this.createLongTermForm.get('bbLr')?.value,
+        location: this.createLongTermForm.get('location')?.value,
+        bbOut: '-',
+        bbDown: '-',
+        luggageType: LuggageType.LONG_TERM,
+      })
+      .subscribe({
+        next: () => {
+          this.snackbar.open('Long term item created!', 'Thanks', {
+            duration: 5000,
+          });
+          document.location.reload();
+          this.dialog.closeAll();
+        },
+        error: (err: HttpErrorResponse) => {
+          console.error(err);
+          this.snackbar.open('Failed to create, please try again.', 'Okay', {
+            duration: 10000,
+          });
+        },
+      });
   }
 }
