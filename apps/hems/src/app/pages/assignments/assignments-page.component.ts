26bbfb6b695bdcdc28a1f3592c937e17ffa02460
Teodor25 <teodor_jonasson@hotmail.com>
2022-11-29 09:59:58 +0100

    refactor: 💡 Move css away from table to global

--- apps/hems/src/app/pages/assignments/assignments-page.component.ts
+++ apps/hems/src/app/pages/assignments/assignments-page.component.ts
@@ -9,7 +9,7 @@ import { UpdateAssignmentDialogComponent } from './updateAssignmentDialog/update
 @Component({
   selector: 'hems-assignments-page',
   templateUrl: './assignments-page.component.html',
-  styleUrls: ['./assignments-page.component.scss'],
+  styleUrls: ['./assignments-page.component.scss', '../../../assets/table.scss'],
 })
 export class AssignmentsPageComponent implements OnInit {
   assignmentList: IAssignment[] = [];
