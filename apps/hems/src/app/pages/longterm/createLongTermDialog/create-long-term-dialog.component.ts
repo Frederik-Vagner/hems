b0dcde02362520ac9f5e3082acd2be8eccf24f51
Teodor25 <teodor_jonasson@hotmail.com>
2022-11-29 06:27:24 +0100

    feat: 🎸 create long term dialog

--- apps/hems/src/app/pages/longterm/createLongTermDialog/create-long-term-dialog.component.ts
+++ apps/hems/src/app/pages/longterm/createLongTermDialog/create-long-term-dialog.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  UntypedFormControl,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
+import { LuggageService } from '../../../services/luggage.service';
 
 @Component({
   selector: 'hems-create-long-term-dialog',
@@ -9,7 +15,37 @@ import { Component, OnInit } from '@angular/core';
   ],
 })
 export class CreateLongTermDialogComponent implements OnInit {
+  createLongTermForm = new UntypedFormGroup({});
+  guestHasApproved = false;
+  maxDatetime = new Date();
+
+  constructor(private luggageService: LuggageService) {}
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.createLongTermForm = new UntypedFormGroup({
+      dateIn: new UntypedFormControl('', [Validators.required]),
+      room: new UntypedFormControl('', [
+        Validators.required,
+        Validators.maxLength(10),
+        Validators.pattern('^[0-9]*$'),
+      ]),
+      bags: new UntypedFormControl('', [Validators.required]),
+      comments: new UntypedFormControl('', [Validators.required]),
+      tagNr: new UntypedFormControl('', [Validators.required]),
+      dateNeeded: new UntypedFormControl('', []),
+      bbLr: new UntypedFormControl('', [Validators.required]),
+      location: new UntypedFormControl('', [Validators.required]),
+      bbOut: new UntypedFormControl('', [Validators.required]),
+      dateOut: new UntypedFormControl('', []),
+    });
+  }
+
+  onSubmit(): void {
+    console.log('skrt');
+  }
+
+  createLongTermEntry(): void {
+    // this.luggageService.create({
+    // }).subscribe()
   }
 }
