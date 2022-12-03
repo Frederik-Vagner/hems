07ea98e648584c093c92f3a905c231f163269072
CristiPV <cristi.valentin29@gmail.com>
2022-12-03 19:30:18 +0100

    update index files

--- libs/interfaces/src/index.ts
+++ libs/interfaces/src/index.ts
@@ -4,6 +4,7 @@ export * from './lib/luggage/luggage.dto';
 export * from './lib/car/car.dto';
 export * from './lib/assignment/assignment.dto';
 export * from './lib/task/task.dto';
+export * from './lib/document/document.dto';
 
 // Interfaces
 export * from './lib/auth.interface';
@@ -14,6 +15,7 @@ export * from './lib/luggage/luggage.interface';
 export * from './lib/task/task.interface';
 export * from './lib/car/car.interface';
 export * from './lib/assignment/assignment.interface';
+export * from './lib/document/document.interface';
 
 // Enums
 export * from './lib/role.enum';
@@ -23,3 +25,4 @@ export * from './lib/luggage/luggage-sort-options.enum';
 export * from './lib/car/car-sort-options.enum';
 export * from './lib/task/task-sort-options.enum';
 export * from './lib/assignment/assignment-sort-options.enum';
+export * from './lib/document/document-sort-options.enum';
