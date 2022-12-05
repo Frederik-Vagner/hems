93ed1a951163208684b0a4dba411f48fe6ad6fa1
Kwandes <bz.jasiek@gmail.com>
2022-12-05 01:39:02 +0100

    Fix .env config not being properly loaded

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AssignmentsModule } from './assignments/assignments.module';
 import { AuthModule } from './auth/auth.module';
@@ -15,6 +16,7 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ envFilePath: ['../.env', '.env'] }),
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
     ScheduleModule.forRoot(),
     AuthModule,
