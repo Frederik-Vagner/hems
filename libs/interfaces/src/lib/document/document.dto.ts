f9b8fd33c5f34ae36c30091fa7788fd6619b782a
Kwandes <bz.jasiek@gmail.com>
2022-12-06 13:07:36 +0100

    Add file signedUrl to getDocumentById endpoint

--- libs/interfaces/src/lib/document/document.dto.ts
+++ libs/interfaces/src/lib/document/document.dto.ts
@@ -1,7 +1,31 @@
 import { ApiModelProperty } from '@nestjs/swagger/dist/decorators/api-model-property.decorator';
 import { IsDateString, IsNotEmpty, IsOptional } from 'class-validator';
-import { ICreateDocumentRequest } from './document.interface';
 import { IUpdateAssignmentRequest } from '../assignment/assignment.interface';
+import { CompletedAtResponse } from '../base.dto';
+import { ICreateDocumentRequest, IDocument } from './document.interface';
+
+export class GetDocumentResponse
+  extends CompletedAtResponse
+  implements IDocument
+{
+  @ApiModelProperty({ example: '123e4567-e89b-12d3-a456-426614174000' })
+  documentId!: string;
+
+  @ApiModelProperty({ example: 'Title stuff' })
+  title!: string;
+
+  @ApiModelProperty({ example: 'This is such interesting data.' })
+  comments?: string;
+
+  @ApiModelProperty({ example: new Date() })
+  lastViewedAt?: Date;
+
+  @ApiModelProperty({ example: true, default: false })
+  showOnDashboard!: boolean;
+
+  @ApiModelProperty({ example: 'declarationOfIndepenence.pdf' })
+  documentName!: string;
+}
 
 export class CreateDocumentRequest implements ICreateDocumentRequest {
   @ApiModelProperty({ example: 'Title stuff' })
@@ -44,4 +68,12 @@ export class UpdateDocumentRequest implements IUpdateAssignmentRequest {
 export class DeleteDocumentResponse {
   @ApiModelProperty({ example: 'Deleted.' })
   message = 'Deleted.';
+}
+
+export class GetDocumentByIdResponse extends GetDocumentResponse {
+  @ApiModelProperty({
+    example:
+      'https://eu-central-1.linodeobjects.com:443/hems-documents/21234.pdf?Signature=XEiYEET1C4T3I25s0I5K1IOH%2Co%3X&Expires=1670271241&AWSAccessKeyId=123456789EAEA',
+  })
+  downloadUrl!: string;
 }
