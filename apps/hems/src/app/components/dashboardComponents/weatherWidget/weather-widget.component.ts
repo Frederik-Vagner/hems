64103a3f3d38b26063ecec461a9fceb3b20d42db
Teodor25 <teodor_jonasson@hotmail.com>
2022-12-08 13:34:16 +0100

    feat: 🎸 display current temp, wind, max and min

--- apps/hems/src/app/components/dashboardComponents/weatherWidget/weather-widget.component.ts
+++ apps/hems/src/app/components/dashboardComponents/weatherWidget/weather-widget.component.ts
@@ -6,28 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather-widget.component.scss'],
 })
 export class WeatherWidgetComponent implements OnInit {
-    weatherData: any;
+  weatherData: any;
+  forecastData: any;
 
-    ngOnInit() {
-        this.getWeatherData()
-    }
+  city = 'Copenhagen';
 
-    getWeatherData(): void {
-        fetch('https://api.openweathermap.org/data/2.5/weather?q=copenhagen&appid=ec87279630ccc1c41b862571e615ab5b')
-        .then(response=>response.json())
-        .then(data=>{this.setWeatherData(data);})
-        .then(() => console.log(this.weatherData))
-    }
+  currentTemp = '2';
+  currentMax = '';
+  currentMin = '';
+  currentWeather = 'sun';
+  currentWind = '13';
 
-    setWeatherData(data: any){
-        this.weatherData = data;
-        const sunsetTime = new Date(this.weatherData.sys.sunset * 1000);
-        this.weatherData.sunset_time = sunsetTime.toLocaleTimeString();
-        const currentDate = new Date();
-        this.weatherData.isDay = (currentDate.getTime() < sunsetTime.getTime());
-        this.weatherData.temp_celcius = (this.weatherData.main.temp - 273.15).toFixed(0);
-        this.weatherData.temp_min = (this.weatherData.main.temp_min - 273.15).toFixed(0);
-        this.weatherData.temp_max = (this.weatherData.main.temp_max - 273.15).toFixed(0);
-        this.weatherData.temp_feels_like = (this.weatherData.main.feels_like - 273.15).toFixed(0);
-      }
+  ngOnInit() {
+    this.getWeatherData();
+    this.getForecastData();
+  }
+
+  getWeatherData(): void {
+    fetch(
+      'https://api.openweathermap.org/data/2.5/weather?q=copenhagen&appid=ec87279630ccc1c41b862571e615ab5b&units=metric'
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        this.setWeatherData(data);
+      })
+      .then(() => console.log(this.weatherData, this.weatherData.main));
+  }
+
+  getForecastData(): void {
+    fetch('https://api.openweathermap.org/data/2.5/forecast?lat=55.6759&lon=12.5655&appid=ec87279630ccc1c41b862571e615ab5b')
+    .then((response) => response.json())
+      .then((data) => {
+        this.forecastData = data;
+      })
+      .then(() => console.log(this.forecastData));
+  }
+
+  setWeatherData(data: any) {
+    this.weatherData = data;
+    const sunsetTime = new Date(this.weatherData.sys.sunset * 1000);
+    this.weatherData.sunset_time = sunsetTime.toLocaleTimeString();
+
+    this.currentTemp = parseFloat(this.weatherData.main.temp).toFixed(0)
+    this.currentMax = parseFloat(this.weatherData.main.temp_max).toFixed(0)
+    this.currentMin = parseFloat(this.weatherData.main.temp_min).toFixed(0)
+
+    this.currentWind = parseFloat(this.weatherData.wind.speed).toFixed(0)
+
+    this.currentWeather = this.weatherData.weather[0].description
+  }
 }
