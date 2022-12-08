4b9131abf4b0398f4d2a41101423ade1394ffa8f
Teodor25 <teodor_jonasson@hotmail.com>
2022-12-08 12:27:37 +0100

    feat: 🎸 initiate dashboard weather widget

---
+++ apps/hems/src/app/components/dashboardComponents/weatherWidget/weather-widget.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hems-weather-widget',
+  templateUrl: './weather-widget.component.html',
+  styleUrls: ['./weather-widget.component.scss'],
+})
+export class WeatherWidgetComponent {
+}
