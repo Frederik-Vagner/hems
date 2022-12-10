b9858fc4cf25b329fa4e62ae5d4314bdd0f5c349
Kwandes <bz.jasiek@gmail.com>
2022-12-10 20:04:15 +0100

    Hardcode tasks data

--- apps/seeder/src/services/tasks.service.ts
+++ apps/seeder/src/services/tasks.service.ts
@@ -25,8 +25,11 @@ export class TasksSeederService {
 
   private getSeedingTasks() {
     const startDate = new Date(Date.now());
-    startDate.setMonth(startDate.getMonth() - 3);
-
-    return this.generator.getTasksForPeriod(startDate, new Date(Date.now()));
+    startDate.setMonth(startDate.getMonth() - 3); // day three months in the past
+    const endDate = new Date(Date.now());
+    endDate.setTime(endDate.getTime() - 24 * 60 * 60 * 1000); // day before today
+    const tasks = this.generator.getTasksForPeriod(startDate, endDate); // completed tasks set in the past
+    tasks.push(...this.generator.getDailyTasks(new Date(Date.now()), false)); // new set of uncompleted tasks for the current date
+    return tasks;
   }
 }
---
+++ libs/task-generator/src/lib/tasks.constant.ts
@@ -0,0 +1,147 @@
+export const listNames = ['Morning', 'Evening', 'SomeOther'];
+
+export const morningTasks = [
+  {
+    time: 'kl 7:00',
+    task: 'Sign-in your radio. Take out bench, ashtray, (refresh sand if needed) & bikes.',
+  },
+  {
+    time: 'kl 7:00',
+    task: 'Plan car pick-up and check which cars need to have their parking extended. Make sure the plates are correctly introduced into the system.',
+  },
+  {
+    time: 'kl 7:00',
+    task: 'Check that you have received the arrival and departure lists, the morning report from the night team, and the Transportation Sheet from the Concierge Organizer. Read it all carefully and prepare yourself for the day. Always share relevant info with others.',
+  },
+  {
+    time: 'kl 7:00',
+    task: 'Set up (upper) lobby & Gallery, Light the candles (change batteries), light the fireplace (fall/winter months), change newspapers, stack up on city maps, purple HDA booklets and magazines. Puff the pillows, correct the chairs and wipe down the tables.',
+  },
+  {
+    time: 'kl 7:15',
+    task: `Clean in front of the hotel (cigarette butts, bottles, and other trash).
+    Check from the corner at Strøget and all the way tot he back entrance in NY Østergade - please check the plants as well. If necessary, take the lead blower or the outdoor vacuum. In case of vomit or animal feces, remove with shovel, big broom (rinse afterwards) and hot water, finally spray any unwanted weeds. Make sure Queen's Gate is clean and tidy (especially the ashtrays). If alone on the AM-shift, wait until the next Bell Man comes in, so that you can do it thoroughly. & empty the ashtrays outside Baltazar, the Queens Gate, on the side of the Main Entrance, by the Staff Entrance.
+    `,
+  },
+  {
+    time: 'kl 7:30',
+    task: `Pick up 2-4 bikes (depends on how busy we are) from the back yard. Place them nicely in front of the hotel Park the bicycles in order and put the keys in the safe & double check that the e-bikes are fully charged.`,
+  },
+  {
+    time: 'kl 7:30',
+    task: `Set up the hospitality Lounge: turn on the TV (always on the news channel) and place todays newspapers (one of each).`,
+  },
+  {
+    time: 'kl 08:00',
+    task: `The flags need to be put up at sun rise. Check the times in the flag list in the bell folder. In case of heavy storm, use the appropriate flags, if at all.`,
+  },
+  {
+    time: 'kl 10:00',
+    task: `Make sure all luggage from departures and arrivals are noted in the book. Check that all the Apple TVs are up to date and the rent note is filled out.`,
+  },
+  {
+    time: 'kl 10:15',
+    task: `Polish the luggage trolleys and the brass door handles and windows in the entrance.`,
+  },
+  {
+    time: 'kl 10:30',
+    task: `Check the lobby and Gallery - lights, pillows and remove glasses and so forth.`,
+  },
+  {
+    time: 'kl 14:00',
+    task: `Check the lobby and Gallery - lights, pillows and remove glasses and so forth.`,
+  },
+  {
+    time: 'kl 14:30',
+    task: `Go through the luggage list and double check with the luggage in LR & LR1 that all are accounted for. Coordinate with Reception which stored luggage can be brought up to cleaned rooms.`,
+  },
+  {
+    time: 'kl 15:00',
+    task: `Reset/clean up bell cabinets, take away any cups, trash and put back the stuff that has been used throughout the day. Prepare and pass on the handover to the PM-shift Remember to write down any relevant info throughout the day.`,
+  },
+];
+
+export const eveningTasks = [
+  {
+    time: 'kl 15:00',
+    task: `Get a radio and sign in on the sheet & get Hand Over from AM-shift.`,
+  },
+  {
+    time: 'kl 15:30',
+    task: `Clean outside the hotel all around the corner (cigarette buds, bottles, paper etc).`,
+  },
+  {
+    time: 'kl 17:00',
+    task: `Put stamps on and send the mail of the day (the postbox at Strøget is emptied at 5PM). Check for packages in the back yard.`,
+  },
+  {
+    time: 'kl 18:00',
+    task: `Polish the luggage trolleys and the brass door handles and windows in the entrance. Clean up in both luggage rooms.`,
+  },
+  {
+    time: 'kl 18:00',
+    task: `Check the lobby & Gallery.`,
+  },
+  {
+    time: 'kl 15:30-21:00',
+    task: `Take the flags down. Check the times on the list in the Bell Folder.`,
+  },
+  {
+    time: 'kl 21:00',
+    task: `Go through the luggage list and double check with the luggage in LR & LR1 that all are accounted for.`,
+  },
+  {
+    time: 'kl 22:00',
+    task: `Move all the bikes (you can leave 2 in the door area) to the backyard and check that they are placed correctly (they need to be within the yellow lines) and lock them. Check that we have all bikes (12+ 5 special) & Put the 2 e-bikes' batteries for charging in LR.`,
+  },
+  {
+    time: 'kl 22:00',
+    task: `Make the Valet Log for the next day and hand it over to the Night Team.`,
+  },
+  {
+    time: 'kl 22:00',
+    task: `Check the lobby & Gallery - the candles, tidy up the pillows and so on.`,
+  },
+  {
+    time: 'kl 22:30-23:00',
+    task: `Assemble/staple together 'Evening Report (Morning Report (front), AM & PM-checklist and leave it in tray Valet Log, luggage list, Room Assignment-sheets & Long Term-sheets (when full) goes in the appropriate binder... print new ones if it is needed and prepare the Bell Binder for the AM-shift.`,
+  },
+  {
+    time: 'kl 22:30-23:00',
+    task: `Make sure fireplace is off & check the lobby & Gallery one last time.`,
+  },
+  {
+    time: 'kl 22:30-23:00',
+    task: `Moved umbrellas from the rack to the closet and fill up with water.`,
+  },
+  {
+    time: 'kl 22:30-23:00',
+    task: `Turn of the computer.`,
+  },
+  {
+    time: 'kl 22:30-23:00',
+    task: `Check that all radios are charging and turned off. Check that all the radios that has been signed for have been returned.`,
+  },
+  {
+    time: 'kl 22:30-23:00',
+    task: `Close the Hospitality Lounge (turn off TV and remove newspapers).`,
+  },
+  {
+    time: 'kl 22:30-23:00',
+    task: `Clean the Bell Stand and cabinets, turn the lights off and lock the cabinets.`,
+  },
+  {
+    time: 'kl 22:30-23:00',
+    task: ``,
+  },
+  {
+    time: 'kl 22:30-23:00',
+    task: `Put the luggage trolleys in, put the bench behind the menu card and take the mats in.`,
+  },
+  {
+    time: 'kl 23:00',
+    task: `Handover with Night Managers (Valet Log and relevant info to team).`,
+  },
+];
+
+export const sampleInitials = ['CV', 'TV', 'TJ', 'CPV', 'CBJ', 'ATV', 'FV'];
