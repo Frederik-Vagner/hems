807ad582ad36076c903316df1f9904b6a95ffcc1
Teodor25 <teodor_jonasson@hotmail.com>
2022-12-12 16:08:30 +0100

    feat: 🎸 add service and most of functionality to create docs

--- apps/hems/src/app/pages/documents/createDocumentDialog/create-document-dialog.component.ts
+++ apps/hems/src/app/pages/documents/createDocumentDialog/create-document-dialog.component.ts
@@ -4,7 +4,10 @@ import {
   UntypedFormControl,
   Validators,
 } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { IDocument } from '@hems/interfaces';
+import { DocumentsService } from '../../../services/documents.service';
 
 @Component({
   selector: 'hems-create-document-dialog',
@@ -23,7 +26,11 @@ export class CreateDocumentDialogComponent implements OnInit {
   @ViewChild('title') titleInput!: ElementRef;
   @ViewChild('comments') commentsInput!: ElementRef;
 
-  constructor(private snackBar: MatSnackBar) {}
+  constructor(
+    private snackBar: MatSnackBar,
+    private documentService: DocumentsService,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.createDocumentForm = new UntypedFormGroup({
@@ -37,7 +44,7 @@ export class CreateDocumentDialogComponent implements OnInit {
 
   onSubmit(): void {
     if (!this.createDocumentForm.valid) {
-      if (this.createDocumentForm.get('room')?.invalid) {
+      if (this.createDocumentForm.get('title')?.invalid) {
         this.titleInput.nativeElement.focus();
       } else if (this.createDocumentForm.get('comments')?.invalid) {
         this.commentsInput.nativeElement.focus();
@@ -49,5 +56,34 @@ export class CreateDocumentDialogComponent implements OnInit {
 
   createDocument(): void {
     this.isLoading = true;
+    const doc: IDocument = {
+      documentId: 'placeholder',
+      title: this.createDocumentForm.get('title')?.value,
+      comments: this.createDocumentForm.get('comments')?.value,
+      showOnDashboard: this.showOnDashboard,
+      documentName: 'skrt',
+    }
+
+    console.log(doc);
+    
+    this.documentService.createDocument(doc).subscribe({
+      next: () => {
+        this.snackBar.open('Bike added!', 'Thanks', { duration: 5000 });
+        document.location.reload();
+        this.dialog.closeAll();
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.snackBar.open(
+          'Failed to upload document, please try again.',
+          'Okay',
+          {
+            duration: 15000,
+          }
+        );
+        this.isLoading = false;
+      },
+    });
   }
 }
